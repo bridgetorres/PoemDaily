b4391fd4c0e870a47a3dd9a69b6664bd2ed477f6
lostxx <42008530+lostxx@users.noreply.github.com>
2018-08-01 14:30:19 -0400

    Updating more to the codes .
    This is going to be long project.

---
+++ poemdaily.js
@@ -0,0 +1,10 @@
+var poems= ["To those that would listen There's nothing left of that night,but the sound of my fingers that runs down your pictured face. And if you were to say I touched you, then the bait is surely drowned for the excuse I use is rarely legal .And before your mothers shawl I looked through a window for a shrouded man founding only good and praise with certainty. But where your long legged flesh stood alone at the door of my home,where my long heavy hearts breath inhaled just as a slow heave of sea bares it's chest.I rose up just as a storm that vents its clawsto rage upon your fair pale young skin, skin adorned with the feathers of a small bird that lit up my eyes and heart for this was my thirst.There is nothing left of that night.The drift has all but disappeared before I could be pitied as he that fell twice.And if you were to say I touched you,on my mortality and bleached white bones I'd say I was rich in my rudeness to those who would listen", "hi" ]
+var index= 0;
+  myArray[i] += 1;
+  myArray.filter((x) => { return x > 5; })
+function nameofFunction (){
+
+}
+<script type="text/javascript">
+alert("Hello world");
+</script>
